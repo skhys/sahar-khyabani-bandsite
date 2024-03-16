166d1f8636e8a35bf6d9e720c54522d219560df6
Sahar Khyabani <saharkhyabani@Sahars-MacBook-Air.local>
2024-03-16 16:07:34 -0700

    responsive shows page mostly done

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,11 +1,15 @@
+// testing console below
+console.log("hello");
+
+// comment box code
 const commentForm = document.getElementById("commentForm");
 const dynamicContent = document.getElementById("dynamicContent");
-console.log(form);
 
 commentForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const commentInputVal = event.target.commentInput.value;
+  const userName = event.target.nameInput.value;
+  const newComment = event.target.commentInput.value;
   const listElement = document.createElement("li");
-  listElement.innerText = commentInputVal;
+  listElement.innerText = userName;
   dynamicContent.appendChild(listElement);
 });
