827f6b15327896a5341d2547669b6fefdf5e7360
Sahar Khyabani <saharkhyabani@Sahars-MacBook-Air.local>
2024-03-17 19:03:33 -0700

    lots of js changes

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -3,67 +3,114 @@ const shows = [
     date: "Mon Sept 09 2024",
     venue: "Ronald Lane",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
   {
     date: "Tue Sept 17 2024",
     venue: "Pier 3 East",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
   {
     date: "Sat Oct 12 2024",
     venue: "View Lounge",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
   {
     date: "Sat Nov 16 2024",
     venue: "Hyatt Agency",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
   {
     date: "Fri Nov 29 2024",
     venue: "Moscow Center",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
   {
     date: "Wed Dec 18 2024",
     venue: "Press Club",
     location: "San Francisco, CA",
+    button: "BUY TICKETS",
   },
 ];
 
-const showContainer = document.querySelector(".show");
+/* <main class="show">
+<h2 class="show__title">Shows</h2>
+<div class="show__list">
+  <!-- <p class="show__subtitle">DATE</p>
+  <p class="show__subtitle">VENUE</p>
+  <p class="show__subtitle">LOCATION</p> -->
+</div>
+<!-- append js here -->
+<!-- <span class="show__dateheader">DATE</span>
+<p class="show__date">Mon Sept 09 2024</p>
+<span class="show__venueheader">VENUE</span>
+<p class="show__venue">Ronald Lane</p>
+<span class="show__locationheader">LOCATION</span>
+<p class="show__location">San Francisco, CA</p>
+<button type="submit">BUY TICKETS</button> -->
+</main> */
+
+const showsContainer = document.querySelector(".show__list");
 console.log(showsContainer);
 
 function displayShow(show) {
-  const showElement = document.createElement("div");
-  showElement.classList.add("show__list");
+  //   const containerElement = document.createElement("div");
+  //   showsContainer.classList.add("show__wrapper");
 
   const dateElement = document.createElement("p");
-  dateElement.classList.add("show__date");
-  dateElement.textContent = show.date;
-  show.appendChild(dateElement);
+  dateElement.classList.add("show__dateheader");
+  dateElement.textContent = "DATE";
+  showsContainer.appendChild(dateElement);
 
-  const venueElement = document.createElement("h5");
-  venueElement.classList.add("show__venue");
-  venueElement.textContent = show.venue;
-  venueElement.appendChild(venueElement);
+  const dateTextElement = document.createElement("p");
+  dateTextElement.classList.add("show__date");
+  dateTextElement.textContent = show.date;
+  showsContainer.appendChild(dateTextElement);
 
-  const locationElement = document.createElement("h5");
-  locationElement.classList.add("show__location");
-  locationElement.textContent = show.location;
-  locationElement.appendChild(locationElement);
+  const venueElement = document.createElement("p");
+  venueElement.classList.add("show__venueheader");
+  venueElement.textContent = "VENUE";
+  showsContainer.appendChild(venueElement);
 
-  showContainer.appendChild(showElement);
+  const venueTextElement = document.createElement("p");
+  venueTextElement.classList.add("show__venue");
+  venueTextElement.textContent = show.venue;
+  showsContainer.appendChild(venueTextElement);
+
+  const locationElement = document.createElement("p");
+  locationElement.classList.add("show__locationheader");
+  locationElement.textContent = "LOCATION";
+  showsContainer.appendChild(locationElement);
+
+  const locationTextElement = document.createElement("p");
+  locationTextElement.classList.add("show__location");
+  locationTextElement.textContent = show.location;
+  showsContainer.appendChild(locationTextElement);
+
+  const buttonElement = document.createElement("button");
+  buttonElement.setAttribute("type", "submit");
+  buttonElement.textContent = show.button;
+  showsContainer.appendChild(buttonElement);
 }
 
 function displayShows() {
-  showContainer.innerHTML = "";
+  for (let i = 0; i < shows.length; i++) {
+    displayShow(shows[i]);
+  }
+  //   showsContainer.innerHTML = "";
 
-  shows.forEach((show) => {
-    displayShow(show);
-  });
+  //   shows.forEach((show) => {
+  //     displayShow(show);
+  //   });
 }
 
+// console.log(shows[0]);
+
+displayShows();
+// shows.push({});
+
 // displayShows();
-// shows.push({
-// })
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,73 @@
 // testing console below
 console.log("hello");
 
+const comments = [
+  {
+    name: "Victor Pinto",
+    date: "11/02/2023",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Christina Cabrera",
+    date: "10/28/2023",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Isaac Tadesse",
+    date: "10/20/2023",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+const commentsContainer = document.querySelector(".comment");
+console.log(commentsContainer);
+
+function displayComment(comment) {
+  const nameElement = document.createElement("p");
+  nameElement.classList.add("comment__name");
+  nameElement.textContent = comment.name;
+  commentsContainer.appendChild(nameElement);
+
+  const dateElement = document.createElement("p");
+  dateElement.classList.add("comment__date");
+  dateElement.textContent = comment.date;
+  commentsContainer.appendChild(dateElement);
+
+  const textElement = document.createElement("p");
+  textElement.classList.add("comment__text");
+  textElement.textContent = comment.text;
+  commentsContainer.appendChild(textElement);
+}
+
+function displayComments() {
+  // commentsContainer.innerHTML = "";
+
+  comments.forEach((comment) => {
+    displayComment(comment);
+  });
+  // for (let i = 0; i < comments.length; i++) {
+  //   displayComments(comments[i]);
+  // }
+}
+
+displayComments();
+// function displayName() {
+//   // commentsContainer.innerHTML = "";
+
+//   userName.forEach((name) => {
+//     displayName(name);
+//   });
+//   // for (let i = 0; i < comments.length; i++) {
+//   //   displayComments(comments[i]);
+//   // }
+// }
+
+// const commentElement = (item) => {
+// const commentElement = document.createElement('div');
+// commentElement.classList.add('comment__title');
+// const commentName
+// }
+
 // comment box code
 const commentForm = document.getElementById("commentForm");
 const dynamicContent = document.getElementById("dynamicContent");
@@ -11,5 +78,6 @@ commentForm.addEventListener("submit", function (event) {
   const newComment = event.target.commentInput.value;
   const listElement = document.createElement("li");
   listElement.innerText = userName;
+  listElement.innerText = newComment;
   dynamicContent.appendChild(listElement);
 });
